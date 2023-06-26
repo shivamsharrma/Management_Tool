8221be2516a51674f32c77bf70c854f080d4f773
shivamsharrma <shivamsharma20533@gmail.com>
2023-06-27 00:08:53 +0530

    react setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import {Button} from 'antd';
+
+function App() {
+  return (
+    <div>
+      <h1>SheyTracker</h1>
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
