5f9286d59b58f8f093c2438ff14b4c4e03dd604a
shivamsharrma <shivamsharma20533@gmail.com>
2023-07-06 13:12:38 +0530

    profile index.js update

---
+++ client/src/pages/Profile/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import { Tabs } from "antd";
+// import Projects from "./Projects";
+
+function Profile() {
+  return (
+    // <Tabs defaultActiveKey="1">
+    //   <Tabs.TabPane tab="Projects" key="1">
+    //     <Projects />
+    //   </Tabs.TabPane>
+    //   <Tabs.TabPane tab="General" key="2">
+    //     General
+    //   </Tabs.TabPane>
+    // </Tabs>
+    <h1>Profile</h1>
+  );
+}
+
+export default Profile;
