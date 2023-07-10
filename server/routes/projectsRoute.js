c8445beee833badd45d8bbe4c24d4e6db98707fc
shivamsharrma <shivamsharma20533@gmail.com>
2023-07-10 11:47:02 +0530

    project delete task complete

--- server/routes/projectsRoute.js
+++ server/routes/projectsRoute.js
@@ -26,7 +26,7 @@ router.post("/get-all-projects", authMiddleware, async (req, res) => {
   try {
     const filters=req.body.filters;
     // const projects = await Project.find({owner: req.body.userId,}).sort({ createdAt: -1 });
-    const projects = await Project.find(filters||{});
+    const projects = await Project.find(filters||{}).sort({createdAt:-1});
 
     res.send({
       success: true,
