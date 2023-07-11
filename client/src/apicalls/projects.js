c6cebe15309a21b1ae0f7a35701db0d5a0d107f5
shivamsharrma <shivamsharma20533@gmail.com>
2023-07-11 10:30:45 +0530

    project info complete

--- client/src/apicalls/projects.js
+++ client/src/apicalls/projects.js
@@ -4,8 +4,14 @@ export const CreateProject = async (project) => apiRequest("post", "/api/project
 
 export const GetAllProjects = async (filters) => apiRequest("post", "/api/projects/get-all-projects", filters);
 
+export const GetProjectById = async (id) => apiRequest("post", "/api/projects/get-project-by-id", { _id: id });
+
 export const EditProject = async (project) => apiRequest("post", "/api/projects/edit-project", project);
 
 export const DeleteProject = async (id) => apiRequest("post", "/api/projects/delete-project", { _id: id });
 
 export const GetProjectsByRole = async (userId) => apiRequest("post", "/api/projects/get-projects-by-role", { userId });
+
+export const AddMemberToProject = async (data) => apiRequest("post", "/api/projects/add-member", data);
+
+export const RemoveMemberFromProject = async (data) => apiRequest("post", "/api/projects/remove-member", data);
--- server/routes/projectsRoute.js
+++ server/routes/projectsRoute.js
@@ -40,6 +40,45 @@ router.post("/get-all-projects", authMiddleware, async (req, res) => {
   }
 });
 
+// get project by id
+router.post("/get-project-by-id", authMiddleware, async (req, res) => {
+  try {
+    const project = await Project.findById(req.body._id)
+      .populate("owner")
+      .populate("members.user");
+    res.send({
+      success: true,
+      data: project,
+    });
+  } catch (error) {
+    res.send({
+      error: error.message,
+      success: false,
+    });
+  }
+});
+
+// get projects by role
+router.post("/get-projects-by-role", authMiddleware, async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    const projects = await Project.find({ "members.user": userId })
+      .sort({
+        createdAt: -1,
+      })
+      .populate("owner");
+    res.send({
+      success: true,
+      data: projects,
+    });
+  } catch (error) {
+    res.send({
+      error: error.message,
+      success: false,
+    });
+  }
+});
+
 // edit a project
 router.post("/edit-project", authMiddleware, async (req, res) => {
   try {
