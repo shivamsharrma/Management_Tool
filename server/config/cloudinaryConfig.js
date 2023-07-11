0a9fe2aaed0e61a3845263dc10e8734cd230d670
shivamsharrma <shivamsharma20533@gmail.com>
2023-07-12 01:35:31 +0530

    final submit

--- server/config/cloudinaryConfig.js
+++ server/config/cloudinaryConfig.js
@@ -2,10 +2,9 @@ const cloudinary = require('cloudinary').v2;
 
 
 // Configuration 
-cloudinary.config({
+cloudinary.config({ 
   cloud_name: process.env.cloudinary_cloud_name,
   api_key: process.env.cloudinary_api_key,
   api_secret: process.env.cloudinary_api_secret,
 });
-
 module.exports = cloudinary;
